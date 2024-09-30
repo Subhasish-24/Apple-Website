3fb6015204edf4cfac7b3da7d6e7cf4b6d5e94d4
Subhasish-24 <gunidd243@gmail.com>
2024-09-30 16:22:59 +0530

    model error

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -51,15 +51,15 @@ const Model = () => {
     gsap.to("#heading", { y: 0, opacity: 1 });
   }, []);
 
-  useGSAP(() => {
-    gsap.to("#heading", { y: 0, opacity: 1 });
-  }, []);
+  // useGSAP(() => {
+  //   gsap.to("#heading", { y: 0, opacity: 1 });
+  // }, []);
 
   return (
     <section className="common-padding">
       <div className="screen-max-width">
         <h1 id="heading" className="section-heading">
-          Take a closer look.
+          Take a closer look:-
         </h1>
 
         <div className="flex flex-col items-center mt-5">
